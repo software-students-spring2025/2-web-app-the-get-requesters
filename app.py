5e3b8bca385c542bbef0d01a3434dc1ea770039d
lanadavydov <lanadavy03@gmail.com>
2025-03-01 23:14:16 -0500

    Implement 'Create new Event' and 'Your Event Details' Screens

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pymongo
 import os
 from dotenv import load_dotenv
 from bson.objectid import ObjectId
+from datetime import datetime, timezone
 
 load_dotenv()
 
@@ -96,6 +97,52 @@ def home():
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+@app.route('/create_event', methods=['GET', 'POST'])
+@login_required
+def create_event():
+    if request.method == 'POST':
+        event_name = request.form.get("event_name")
+        event_date = request.form.get("event_date")
+        description = request.form.get("description")
+        invitees = request.form.get("invitees").split(",")
+        event_creator = current_user.username
+
+        try:
+            from datetime import datetime
+            event_timestamp = int(datetime.strptime(event_date,"%Y-%m-%d").timestamp())
+
+            db.events.insert_one({
+                "event_name": event_name,
+                "date": event_timestamp,
+                "description": description,
+                "invitees":[user.strip() for user in invitees],
+                "creator": event_creator
+            })
+
+            flash("Event created successfully!", "success")
+            return redirect(url_for('home'))
+        
+        except Exception as e:
+            flash(f"Error: {str(e)}", "danger")
+            return redirect(url_for('create_event')) 
+    return render_template("create_event.html")
+
+@app.route('/event/<event_id>')
+@login_required
+def your_event_details(event_id):
+    event = db.events.find_one({"_id": ObjectId(event_id)})
+
+    if not event or event["creator"] != current_user.username:
+        flash("You are not authorized to view this event.", "danger")
+        return redirect(url_for('home'))
+    
+    from datetime import datetime
+
+    event["date_display"] = datetime.fromtimestamp(event["date"], tz=timezone.utc).strftime('%B %d, %Y')
+    comments = list(db.comments.find({"event_id": event_id}))
+    
+    return render_template("your_event_details.html", event=event, comments=comments)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ.get('PORT', 5001), debug=False)
